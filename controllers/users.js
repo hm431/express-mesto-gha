c52c965b393d775f8aae5e7b3d19f49dfbb7ad03
Andrey Chuchalin <aic7120@gmail.com>
2024-01-31 00:53:51 +0300

    fixes after revey

--- controllers/users.js
+++ controllers/users.js
@@ -13,7 +13,7 @@ module.exports.getUsers = (req, res) => {
 
 module.exports.getIdUsers = (req, res) => {
   const { userId } = req.params;
-  User.findById.orFail(userId)
+  User.findById(userId).orFail
     .then((user) => {
      return res.send({ user });
     })
