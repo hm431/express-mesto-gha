6fd15c030594ed86b0c0afa97f18c92ea78223f2
Andrey Chuchalin <aic7120@gmail.com>
2024-02-02 06:53:06 +0300

    Исправление ошибок  ответов  для ПР14

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,4 +1,6 @@
-const jwt = require('jsonwebtoken');
+/*const jwt = require('jsonwebtoken');
+
+
 
 const handleAuthError = (res) => {
   res
@@ -31,4 +33,32 @@ module.exports = (req, res, next) => {
   req.user = payload; // записываем пейлоуд в объект запроса
 
   return next(); // пропускаем запрос дальше
+}; */
+
+
+const jwt = require('jsonwebtoken');
+
+//const { SECRET_SIGNING_KEY } = require('../utils/constants');
+const UnauthorizedError = require('../errors/UnauthorizedError');
+
+module.exports = (req, _, next) => {
+  const { authorization } = req.headers;
+  const bearer = 'Bearer ';
+
+  if (!authorization || !authorization.startsWith(bearer)) {
+    return next(new UnauthorizedError('Неправильные почта или пароль'));
+  }
+
+  const token = authorization.replace(bearer, '');
+  let payload;
+
+  try {
+    payload = jwt.verify(token, 'super-strong-secret');
+  } catch (err) {
+    return next(new UnauthorizedError('Неправильные почта или пароль'));
+  }
+
+  req.user = payload;
+
+  return next();
 };
